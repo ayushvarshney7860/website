39804d719c42dd0e1991795ecb93fc92c42c26a4
ayushvarshney7860 <ayush.v7860@gmail.com>
2024-07-08 11:50:43 +0530

    ss

--- app/config/seo-config.ts
+++ app/config/seo-config.ts
@@ -1,16 +1,16 @@
 import { Metadata } from "next";
 
 const seo_config: Metadata = {
-   title: "HighPixelZone",
+   title: "HighPixelZone-Best Wedding Photographer In Lucknow",
    description:
-      "High Pixel Zone is a photography business which can preserve your memories as photos and videos.",
+      "HighPixelZone is the Best Professional Wedding Photographers Lucknow Near me Wedding Photography, Engagement, Pre-wedding, Reception, Anniversary.",
    keywords: ["highpixelzone", "photography", "wedding"],
    openGraph: {
       type: "website",
       url: "https://highpixelzone.com/",
-      title: "High Pixel Zone",
+      title: "HighPixelZone-Best Wedding Photographer In Lucknow",
       description:
-         "High Pixel Zone is a photography business which can preserve your memories as photos and videos.",
+         "HighPixelZone is the Best Professional Wedding Photographers Lucknow Near me Wedding Photography, Engagement, Pre-wedding, Reception, Anniversary.",
       images: [
          {
             url: "https://highpixelzone.com/logo.svg",
